5a31b4187c2e5270ee52c31acf343acb763caa67
shuaiqidekaige <836133817@qq.com>
2025-10-23 00:30:35 +0800

    'init'

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'ink';
+import { App } from './ui/App';
+
+function run() {
+    render(React.createElement(App))
+}
+
+run()
---
+++ src/ui/App.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from 'ink'
+
+export function App() {
+  return (
+    <Box flexDirection='column'>
+      <Text>1234</Text>
+    </Box>
+  )
+}
